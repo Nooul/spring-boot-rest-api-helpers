44844cf2796920848a3ca35650f55b23bd79af2a
Michail Michailidis <mmichaelid@gmail.com>
2019-02-01 09:25:38 +0200

    update wrapper advice to wrap only iterables that are not pages and not single objects as before

--- src/main/java/reactAdmin/rest/controllerAdvices/WrapperAdvice.java
+++ src/main/java/reactAdmin/rest/controllerAdvices/WrapperAdvice.java
@@ -1,18 +1,15 @@
 package reactAdmin.rest.controllerAdvices;
-
-import lombok.Data;
+import lombok.NonNull;
+import lombok.Value;
 import org.springframework.core.MethodParameter;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
-import reactAdmin.rest.entities.ErrorDetails;
 
-import java.util.*;
+import java.util.Arrays;
 
 //https://stackoverflow.com/a/40333275/986160
 @ControllerAdvice
@@ -25,53 +22,24 @@ public class WrapperAdvice implements ResponseBodyAdvice {
     @Override
     @SuppressWarnings("unchecked")
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
-        
-        if (!  ((body instanceof ErrorDetails) ||
-                (body instanceof List) ||
-                (body instanceof Set) ||
-                (body instanceof Page) ||
-                (body instanceof InputStreamResource) ||
-                (body instanceof byte[]) ||
-                (body instanceof LinkedHashMap && ((LinkedHashMap)(body)).containsKey("exception"))
-            ))
-        {
-            return page(body);
+
+        if (isArray(body)) {
+            return new Wrapper(Arrays.asList(body));
         }
-        else if (body instanceof List) {
-            return pageList((List)body);
-        }
-        else if(body instanceof Set) {
-            return pageSet((Set)body);
+        if (body instanceof Iterable && !(body instanceof Page)) {
+            return new Wrapper((Iterable)body);
         }
         return body;
     }
 
+    @Value
+    private class Wrapper {
+        private final @NonNull Iterable content;
 
-    public static <T> Page<T> page(T obj) {
-        if (obj == null) {
-            return new PageImpl<>(new ArrayList<>());
-        }
-        return new PageImpl<>(new ArrayList<>(Arrays.asList(obj)));
     }
 
-    public static <T> Page<T> pageList(List<T> list) {
-        if (list == null) {
-            return new PageImpl<>(new ArrayList<>());
-        }
-        return new PageImpl<>(new ArrayList<>(list));
-    }
-
-    public static <T> Page<T> pageSet(Set<T> set) {
-        if (set == null) {
-            return new PageImpl<>(new ArrayList<>());
-        }
-        return new PageImpl<>(new ArrayList<>(set));
-    }
-
-    public static <T> Page<T> pagePage(Page page) {
-        if (page == null) {
-            return new PageImpl<>(new ArrayList<>());
-        }
-        return page;
+    public static boolean isArray(Object obj)
+    {
+        return obj != null && obj.getClass().isArray();
     }
 }
