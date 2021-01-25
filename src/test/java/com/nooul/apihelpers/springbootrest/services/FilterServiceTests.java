60dd6d3f8b854adeaa60665427db2d7bd5241519
VasileosK <vasileios@konstantinou.dev>
2021-01-25 12:14:40 +0200

    remove quava dependency and add appache common test

---
+++ src/test/java/com/nooul/apihelpers/springbootrest/services/FilterServiceTests.java
@@ -0,0 +1,25 @@
+package com.nooul.apihelpers.springbootrest.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilterServiceTests {
+
+    private FilterService filterService;
+
+    @BeforeEach
+    void init(){
+        filterService = new FilterService();
+    }
+
+    @Test
+    void given_a_snake_case_string_conversion_should_succeed() {
+        String snakeCase = "this_is_a_snake_case";
+        String camelCase = filterService.convertToCamelCase(snakeCase);
+        Assertions.assertEquals("thisIsASnakeCase", camelCase);
+    }
+
+}
