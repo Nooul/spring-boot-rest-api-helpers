01f26e51e2e9488d753c5335d2a4c49d52126945
VasileosK <vasileios@konstantinou.dev>
2021-01-25 12:21:29 +0200

    clean pom file

--- src/test/java/com/nooul/apihelpers/springbootrest/services/FilterServiceTests.java
+++ src/test/java/com/nooul/apihelpers/springbootrest/services/FilterServiceTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class FilterServiceTests {
 
